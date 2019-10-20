5a4933f6ba8632c2e6d27f5c2e8f8a9afecf32a4
rcardin <riccardo.cardin@gmail.com>
2019-10-20 15:39:09 +0200

    Added duplication programs and add the print of the command name in the first three code snippets

--- chapter1/echo1.go
+++ chapter1/echo1.go
@@ -7,11 +7,9 @@ import (
 
 func main() {
 	var s, sep string
-	for i := 1; i < len(os.Args); i++ {
+	for i := 0; i < len(os.Args); i++ {
 		s += sep + os.Args[i]
 		sep = " "
 	}
 	fmt.Println(s)
 }
-
-
